1686e181b2edd71a7ed23ac8214bc40eba7120e9
Sakna Hewa Galamulage <sakna.hewagalamulage@eskspatial.com.au>
2025-05-26 23:14:54 +1000

    updated the comments of the db_handler

--- data/db_handler.py
+++ data/db_handler.py
@@ -4,10 +4,18 @@ import sqlite3
 
 class DbHandler:
     def __init__(self):
+        """Constructor.
+        SQLite database handler.
+        Used to retrieve and update the keypad data.
+        """
         self.plugin_dir = os.path.dirname(__file__)
         self.db_path = os.path.join(self.plugin_dir, 'keypad_data.sqlite')
 
     def load_keypad_data(self):
+        """Load the keypad data from the database.
+
+        :return: Keypad data
+        """
         with sqlite3.connect(self.db_path) as conn:
             cur = conn.cursor()
             cur.execute("SELECT * FROM categories")
@@ -28,7 +36,13 @@ class DbHandler:
 
             return keypad_data
 
+
     def reset_and_update(self, data):
+        """Reset and update the keypad data in the database.
+
+        :param data: Keypad data
+        :type data: list of dicts
+        """
         with sqlite3.connect(self.db_path) as conn:
             cur = conn.cursor()
             # clearing data
@@ -54,7 +68,13 @@ class DbHandler:
 
             conn.commit()
 
+
     def get_checked_category_items(self):
+        """Get the checked category items from the database.
+
+        :return: Category items
+        :type: list of dicts
+        """
         with sqlite3.connect(self.db_path) as conn:
             cur = conn.cursor()
             query = ("SELECT i.item, c.selected, c.colour FROM items as i "
